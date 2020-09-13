7ea8256e036acf6541571988e1733337276d11b1
Masaki <your.stonesour@gmail.com>
2020-09-13 14:33:20 +0900

    Add loader Add global variables for data and bss section

--- main.c
+++ main.c
@@ -1,9 +1,26 @@
-int GlobalInt = 3;
+#include "main.h"
 
-int main(void) {
+int GlobalInt = 3;
+int GlobalInt2 = 3;
+int GlobalIntUninit;
+
+extern char* _data_load_source;
+extern char* _data_load_size;
+extern char* _data_load_destinatin;
+
+int main() {
+    load_memory();
+
     int a = 0;
     int b = 2;
-    int c = (a + 5) * b;
+    GlobalIntUninit = a + b + GlobalInt;
+    GlobalInt2 = GlobalIntUninit * 2;
 
     return 0;
+}
+
+void load_memory() {
+    for (int i = 0; i < (unsigned int)_data_load_size; i++) {
+        _data_load_destinatin[i] = _data_load_source[i];
+    }
 }
---
+++ main.h
@@ -0,0 +1,2 @@
+int main();
+void load_memory();
