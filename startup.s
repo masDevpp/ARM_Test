15122e4f9ec9b6e1995d6b7598d7ed76c1d80b90
Masaki <your.stonesour@gmail.com>
2020-09-19 14:43:22 +0900

    Setup interrupt handler

--- startup.s
+++ startup.s
@@ -1,20 +1,38 @@
 //.section .text
 
-b   _reset_handler    // reset
-b   _nop_function     // undefined
-b   _nop_function     // softoware
-b   _nop_function     // prefetch abort
-b   _nop_function     // data abort
-b   _nop_function     // not assigned
-b   _nop_function     // IRQ
-b   _nop_function     // FIQ
+b   _reset_handler          // reset
+b   _undefined_instruction_handler // undefined instruction
+b   _software_int_handler   // softoware
+b   _prefetch_abort_handler // prefetch abort
+b   _data_abort_handler     // data abort
+b   _nop_function           // not assigned
+b   _irq_handler            // IRQ
+b   _fiq_handler            // FIQ
 
 _reset_handler:
 movw    sp,  0xfff0      // Set sp to 0x2001fff0
 movt    sp,  0x2001
-bl   main
+bl  main
+b   _nop_function
+
+_undefined_instruction_handler:
+b   _undefined_instruction_handler
+
+_software_int_handler:
+b   _software_int_handler
+
+_prefetch_abort_handler:
+b   _prefetch_abort_handler
+
+_data_abort_handler:
+b   _data_abort_handler
 
 _nop_function:
 b   _nop_function
 
-.end
+_irq_handler:
+b   _irq_handler
+
+_fiq_handler:
+b   _fiq_handler
+
