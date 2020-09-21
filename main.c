9bb3d9026f7936fdc819cd7e83ef9ab93f241212
Masaki <your.stonesour@gmail.com>
2020-09-21 21:07:28 +0900

    Add code to blink LED

--- main.c
+++ main.c
@@ -18,6 +18,21 @@ int main() {
     int b = 2;
     GlobalIntUninit = a + b + GlobalInt;
     GlobalInt2 = GlobalIntUninit * 2;
+    
+    setupLED();
+    
+    uint32 ledOn = 1;
+    while (1) {
+        if (ledOn == 1) {
+            setLEDOn();
+            ledOn = 0;
+        } else {
+            setLEDOff();
+            ledOn = 1;
+        }
+
+        for (int i = 0; i < 1000000; i++);
+    }
 
     return 0;
 }
@@ -33,4 +48,18 @@ void loadMemory() {
         _bss_address[i] = 0;
     }
 }
+void setupLED() {
+    // GPIOA clock enable by RCC_AHB1ENR
+    *((uint32 *)RCC_AHB1ENR) |= RCC_AHB1ENR_GPIOAEN_MASK;
+
+    // LED (LD2) is connected to PA5 of GPIOA
+    *((uint32 *)GPIOA_MODER) |= GPIOA_MODER_PA5_OUTPUT_MODE_MASK;
+}
+
+void setLEDOn() {
+    *((uint32 *)GPIOA_BSRR) |= GPIOA_BSRR_PA5_SET_MASK;
+}
+
+void setLEDOff() {
+    *((uint32 *)GPIOA_BSRR) |= GPIOA_BSRR_PA5_RESET_MASK;
 }
--- main.h
+++ main.h
@@ -4,5 +4,27 @@ typedef short int16;
 typedef unsigned short uint16;
 typedef int int32;
 typedef unsigned int uint32;
+
+// GPIOA
+const uint32 GPIOA        = 0x40020000;
+const uint32 GPIOA_MODER  = GPIOA + 0x00;
+const uint32 GPIOA_OTYPER = GPIOA + 0x04;
+const uint32 GPIOA_ODR    = GPIOA + 0x14;
+const uint32 GPIOA_BSRR   = GPIOA + 0x18;
+
+const uint32 GPIOA_MODER_PA5_OUTPUT_MODE_MASK = 0x00000400;
+const uint32 GPIOA_ODR_PA5_MASK = 0x00000020;
+const uint32 GPIOA_BSRR_PA5_SET_MASK = 0x00000020;
+const uint32 GPIOA_BSRR_PA5_RESET_MASK = 0x00200000;
+
+// RCC
+const uint32 RCC          = 0x40023800;
+const uint32 RCC_AHB1ENR  = RCC + 0x30;
+
+const uint32 RCC_AHB1ENR_GPIOAEN_MASK = 0x00000001;
+
 int main();
-void loadMemory();
+void loadMemory();void setupLED();
+void setupLED();
+void setLEDOn();
+void setLEDOff();
