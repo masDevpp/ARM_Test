0c75b592dc1bd5488edafb5bc0542b1bd6c5eb19
Masaki <your.stonesour@gmail.com>
2020-09-22 15:52:43 +0900

    Add vector handler address to 1 to align thumb instruction call

--- vector.s
+++ vector.s
@@ -5,25 +5,25 @@
 // Refer Cortex-M4 Devices Generic User Guide 2.3.2 Exception types
 // OR STM32F446xx Reference manual (RM0390) 10.1.3 Interrupt and exception vectors
 .word   0
-.word   _reset_handler
-.word   _nmi_handler
-.word   _hard_fault_handler
-.word   _mem_manage_handler
-.word   _bus_fault_handler
-.word   _usage_fault_handler
+.word   _reset_handler + 1
+.word   _nmi_handler + 1
+.word   _hard_fault_handler + 1
+.word   _mem_manage_handler + 1
+.word   _bus_fault_handler + 1
+.word   _usage_fault_handler + 1
 .word   0
 .word   0
 .word   0
 .word   0
-.word   _svc_call_handler
+.word   _svc_call_handler + 1
 .word   0
 .word   0
-.word   _pend_sv_handler
-.word   _sys_tick_handler
+.word   _pend_sv_handler + 1
+.word   _sys_tick_handler + 1
 
 // Vector for STM32 96 interrupts
 // Refer STM32F446 manual 10 Interrupts and events
-.word   _irq_handler
+.word   _irq_handler + 1
 .word   0
 .word   0
 .word   0
