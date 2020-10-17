ea98fc873ae2127ee33faba49f75cb00c3daf8c1
Masaki <your.stonesour@gmail.com>
2020-10-17 18:28:39 +0900

    Add command handle

--- CommandHandler.cpp
+++ CommandHandler.cpp
@@ -1,5 +1,7 @@
 #include "CommandHandler.h"
 #include "SerialHandler.h"
+#include "Kernel.h"
+#include "String.h"
 
 void CommandHandler::Setup() {
     SerialHandler::Setup();
@@ -10,14 +12,34 @@ void CommandHandler::Setup() {
 }
 
 void CommandHandler::Loop() {
-   while (true) {
-       if (SerialHandler::IsReceiveComplete) {
-           SerialHandler::SendString("\n\r");
-           SerialHandler::SendString(SerialHandler::SerialBuffer);
-           SerialHandler::SendString("\n\r>");
-           SerialHandler::IsReceiveComplete = false;
-       }
-   }
+    while (true) {
+        if (!SerialHandler::IsReceiveComplete) continue;
+        
+        SerialHandler::SendString("\n\r");
+        SerialHandler::SendString(SerialHandler::SerialBuffer);
+        SerialHandler::SendString("\n\r");
+
+        String receiveString(SerialHandler::SerialBuffer);
+        String command = nullptr;
+        String parameter = nullptr;
+
+        if (receiveString.IndexOf(' ') > 0) {
+            command = String(SerialHandler::SerialBuffer, receiveString.IndexOf(' '));
+            parameter = String(&SerialHandler::SerialBuffer[receiveString.IndexOf(' ') + 1]);
+        } else {
+            command = receiveString;
+        }
+
+        SerialHandler::SendString(command.GetBuffer());
+        SerialHandler::SendString("\n\r");
+
+        if (command.Equal(String("echo"))) {
+            SerialHandler::SendString(parameter.GetBuffer());
+        }
+
+        SerialHandler::SendString("\n\r>");
+        SerialHandler::IsReceiveComplete = false;
+    }
 }
 
 void CommandHandler::SerialInterrupt() {
