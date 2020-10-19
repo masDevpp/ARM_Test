6db5b2149047d11e5345660c666d216fe0b17b97
Masaki <your.stonesour@gmail.com>
2020-10-19 23:33:40 +0900

    Add CommandParser

--- CommandHandler.cpp
+++ CommandHandler.cpp
@@ -1,7 +1,6 @@
 #include "CommandHandler.h"
 #include "SerialHandler.h"
 #include "Kernel.h"
-#include "String.h"
 #include "Trace.h"
 
 void CommandHandler::Setup() {
@@ -19,35 +18,17 @@ void CommandHandler::Loop() {
             continue;
         }
         
-        Trace::Add("Recv", *(uint32 *)SerialHandler::SerialBuffer);
+        Trace::Add("Recv", ((uint32 *)SerialHandler::SerialBuffer)[0], ((uint32 *)SerialHandler::SerialBuffer)[1]);
 
-        String receiveString(SerialHandler::SerialBuffer);
-        String command;
-        String parameter;
+        CommandParser cp(SerialHandler::SerialBuffer);
 
-        Trace::Add("IdxOfSps", receiveString.IndexOf(' '));
-
-        if (receiveString.IndexOf(' ') > 0) {
-            Trace::Add("Cmd Ptr", *(uint32 *)SerialHandler::SerialBuffer);
-            command = String(SerialHandler::SerialBuffer, receiveString.IndexOf(' '));
-
-            Trace::Add("Parm Ptr", *(uint32 *)&SerialHandler::SerialBuffer[receiveString.IndexOf(' ') + 1]);
-            parameter = String(&SerialHandler::SerialBuffer[receiveString.IndexOf(' ') + 1]);
-        } else {
-            command = receiveString;
-        }
-
-        if (command.Equal("echo")) {
+        if (cp.Command.Equal("echo")) {
             SerialHandler::SendString("\n\r");
-            SerialHandler::SendString(parameter);
+            SerialHandler::SendString(cp.Parameter[0]);
         }
 
         SerialHandler::SendString("\n\r>");
         SerialHandler::IsReceiveComplete = false;
-
-        receiveString.Release();
-        command.Release();
-        parameter.Release();
     }
 }
 
@@ -56,5 +37,63 @@ void CommandHandler::SerialInterrupt() {
 
     if (SerialHandler::LastReceiveByte != 0) {
         SerialHandler::SendByte(SerialHandler::LastReceiveByte);
+    }
+}
+
+CommandParser::CommandParser(const void *data) {
+    char *chars = (char *)data;
+    uint32 index = 0;
+    NumOfParameter = 0;
+    
+    // Trim
+    while (true) {
+        if (chars[index] == '\0') {
+            Command = String("");
+            return;
+        }
+
+        if (chars[index] != ' ') break;
+        index++;
+    }
+
+    // Command string
+    uint32 commandStart = index;
+    while (!(chars[index] == ' ' || chars[index] == '\0')) index++;
+    uint32 commandEnd = index;
+
+    Command = String(&chars[commandStart], commandEnd - commandStart);
+
+    Trace::Add("CP Cmd", *(uint32 *)Command.GetBuffer());
+
+    while (chars[index] == ' ') index++;
+
+    // No parameter
+    if (chars[index] == '\0') return;
+    
+    // Parameter string
+    while (true) {
+        uint32 parameterStart = index;
+        while (!(chars[index] == ' ' || chars[index] == '\0')) index++;
+        uint32 parameterEnd = index;
+
+        Parameter[NumOfParameter] = String(&chars[parameterStart], parameterEnd - parameterStart);
+        
+        Trace::Add("CP Param", *(uint32 *)Parameter[NumOfParameter].GetBuffer());
+        
+        NumOfParameter++;
+
+        // Skip space
+        while (chars[index] == ' ') index++;
+        if (chars[index] == '\0') break;
+
+        if (NumOfParameter == MAX_NUM_PARAMETER) break;
+    }
+}
+
+CommandParser::~CommandParser() {
+    Command.Release();
+    
+    for (uint32 i = 0; i < NumOfParameter; i++) {
+        Parameter[i].Release();
     }
 }
--- CommandHandler.h
+++ CommandHandler.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "DataType.h"
+#include "String.h"
 
 class CommandHandler {
 public:
@@ -7,4 +8,16 @@ public:
     static void Loop();
 
     static void SerialInterrupt();
+};
+
+class CommandParser {
+public:
+    CommandParser(const void *data);
+    ~CommandParser();
+
+    static const uint32 MAX_NUM_PARAMETER = 6;
+    uint32 NumOfParameter;
+
+    String Command;
+    String Parameter[MAX_NUM_PARAMETER];
 };
