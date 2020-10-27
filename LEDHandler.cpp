97db2c739dade42b2858a5d0b186051fe615e795
Masaki <your.stonesour@gmail.com>
2020-10-27 23:06:06 +0900

    Use timer to switch LED and make thread sleep while wait

--- LEDHandler.cpp
+++ LEDHandler.cpp
@@ -1,16 +1,36 @@
 #include "LEDHandler.h"
+#include "Kernel.h"
+#include "Trace.h"
+
+Timer *LEDHandler::LEDTimer;
 
 void LEDHandler::Setup() {
     LED::Setup();
     LED::Set(true);
+
+    LEDTimer = TimerProvider::GetInstance();
+
+    Trace::Add("LEDTimer", (uint32)LEDTimer);
+
+    LEDTimer->Setup(0x0100);
+    LEDTimer->Start();
+
     Loop();
 }
 
 void LEDHandler::Loop() {
-    for (uint32 i = 0; ; i++) {
-        LED::Set(!LED::Get());
+    uint32 i = 0;
+    while (true) {
+        uint32 ledWait = (i < 50) ? BLINK_MS_FAST : BLINK_MS_SLOW;
+        uint32 ledElapseMs = LEDTimer->GetElapseMs();
 
-        int waitLoop = (i < 50) ? 50000 : 100000;
-        for (int i = 0; i < waitLoop; i++);
+        if (ledElapseMs < ledWait) {
+            Kernel::ThreadSleep();
+            continue;
+        } else {
+            LED::Set(!LED::Get());
+            LEDTimer->Reset();
+            i++;
+        }
     }
 }
--- LEDHandler.h
+++ LEDHandler.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "DataType.h"
 #include "LED.h"
+#include "Timer.h"
 
 class LEDHandler {
 public:
     static void Setup();
     static void Loop();
+
+private:
+    static const uint32 BLINK_MS_FAST = 50;
+    static const uint32 BLINK_MS_SLOW = 500;
+
+    static Timer *LEDTimer;
 };
